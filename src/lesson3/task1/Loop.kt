5787d941e115ecbbda04a55ff8f3c475a94ea35e
so-siickk <sawelysawin@yandex.ru>
2022-09-23 21:12:42 +0300

    lesson3_1

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -2,7 +2,8 @@
 
 package lesson3.task1
 
-import kotlin.math.sqrt
+import lesson1.task1.sqr
+import kotlin.math.*
 
 // Урок 3: циклы
 // Максимальное количество баллов = 9
@@ -72,7 +73,15 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int {
+    var count = 0
+    var number = n.toDouble()
+    do {
+        number /= 10
+        count++
+    } while (number >= 1)
+    return count
+}
 
 /**
  * Простая (2 балла)
@@ -80,21 +89,41 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int {
+    var numberFirst = 1
+    var numberSecond = 1
+    var numberSum = 1
+    for (i in 3..n) {
+        numberSum = numberFirst + numberSecond
+        numberSecond = numberFirst
+        numberFirst = numberSum
+    }
+    return numberSum
+}
 
 /**
  * Простая (2 балла)
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
+fun minDivisor(n: Int): Int {
+    for (i in 2..n / 2 + 1) {
+        if (n % i == 0) return i
+    }
+    return n
+}
 
 /**
  * Простая (2 балла)
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    for (i in n / 2 + 1 downTo 2) {
+        if (n % i == 0) return i
+    }
+    return 1
+}
 
 /**
  * Простая (2 балла)
@@ -112,7 +141,16 @@ fun maxDivisor(n: Int): Int = TODO()
  * Написать функцию, которая находит, сколько шагов требуется для
  * этого для какого-либо начального X > 0.
  */
-fun collatzSteps(x: Int): Int = TODO()
+fun collatzSteps(x: Int): Int {
+    var count = 0
+    var number = x.toDouble()
+    while (number != 1.0) {
+        if (number % 2 == 0.0) number /= 2
+        else number = number * 3 + 1
+        count++
+    }
+    return count
+}
 
 /**
  * Средняя (3 балла)
@@ -120,7 +158,12 @@ fun collatzSteps(x: Int): Int = TODO()
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+fun lcm(m: Int, n: Int): Int {
+    for (i in max(m, n) until m * n) {
+        if ((i % m == 0) && (i % n == 0)) return i
+    }
+    return m * n
+}
 
 /**
  * Средняя (3 балла)
@@ -129,7 +172,13 @@ fun lcm(m: Int, n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    for (i in 2..min(m, n))
+        if ((m % i == 0) && (n % i == 0)) {
+            return false
+        }
+    return true
+}
 
 /**
  * Средняя (3 балла)
@@ -138,7 +187,17 @@ fun isCoPrime(m: Int, n: Int): Boolean = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun revert(n: Int): Int = TODO()
+fun revert(n: Int): Int {
+    var numbers = emptyArray<Int>()
+    var oldNumber = n
+    var newNumber = 0.0
+    while (oldNumber > 0) {
+        numbers += oldNumber % 10
+        oldNumber = oldNumber.div(10)
+    }
+    for (i in 0 until numbers.count()) newNumber += numbers[i] * 10.0.pow(numbers.count() - 1 - i)
+    return newNumber.toInt()
+}
 
 /**
  * Средняя (3 балла)
@@ -149,7 +208,18 @@ fun revert(n: Int): Int = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun isPalindrome(n: Int): Boolean = TODO()
+fun isPalindrome(n: Int): Boolean {
+    var numbers = emptyArray<Int>()
+    var oldNumber = n
+    while (oldNumber > 0) {
+        numbers += oldNumber % 10
+        oldNumber = oldNumber.div(10)
+    }
+    for (i in 0 until numbers.count() / 2) {
+        if (numbers[i] != numbers[numbers.count() - 1 - i]) return false
+    }
+    return true
+}
 
 /**
  * Средняя (3 балла)
@@ -159,7 +229,16 @@ fun isPalindrome(n: Int): Boolean = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun hasDifferentDigits(n: Int): Boolean = TODO()
+fun hasDifferentDigits(n: Int): Boolean {
+    var numbers = emptyArray<Int>()
+    var oldNumber = n
+    while (oldNumber > 10) {
+        numbers += oldNumber % 10
+        oldNumber = oldNumber.div(10)
+        if (!numbers.contains(oldNumber % 10)) return true
+    }
+    return false
+}
 
 /**
  * Средняя (4 балла)
@@ -192,7 +271,27 @@ fun cos(x: Double, eps: Double): Double = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun squareSequenceDigit(n: Int): Int = TODO()
+fun squareSequenceDigit(n: Int): Int {
+    var countNumbers = 0
+    var countSquare = 0
+    var square = 0
+    while (n > countNumbers) {
+        square++
+        countSquare = sqr(square)
+        while (countSquare > 0) {
+            countNumbers++
+            countSquare = countSquare.div(10)
+        }
+        if (countNumbers >= n) {
+            countSquare = sqr(square)
+            for (i in 1..countNumbers - n) {
+                countSquare = countSquare.div(10)
+            }
+            return countSquare % 10
+        }
+    }
+    return 0
+}
 
 /**
  * Сложная (5 баллов)
@@ -203,4 +302,28 @@ fun squareSequenceDigit(n: Int): Int = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun fibSequenceDigit(n: Int): Int = TODO()
+fun fibSequenceDigit(n: Int): Int {
+    var fibFirst = 0
+    var fibSecond = 0
+    var fibThird = 1
+    var fibCount = 0
+    var count = 0
+    while (n > count) {
+        fibFirst = fibSecond + fibThird
+        fibThird = fibSecond
+        fibSecond = fibFirst
+        fibCount = fibFirst
+        while (fibCount > 0) {
+            count++
+            fibCount = fibCount.div(10)
+        }
+        if (count >= n) {
+            fibCount = fibFirst
+            for (i in 1..count - n) {
+                fibCount = fibCount.div(10)
+            }
+            return fibCount % 10
+        }
+    }
+    return 0
+}

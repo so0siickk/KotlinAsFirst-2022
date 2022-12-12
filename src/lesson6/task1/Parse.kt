9c4050f04b5a9c73762ee7af41022bb3980be98c
so-siickk <sawelysawin@yandex.ru>
2022-12-12 19:28:16 +0300

    lesson7_1

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -169,39 +169,24 @@ fun firstDuplicateIndex(str: String): Int = TODO()
  * Все цены должны быть больше нуля либо равны нулю.
  */
 fun mostExpensive(description: String): String {
-    var strPrice = ""
-    var maxPrice = 0.0
-    var name = ""
-    var maxName = ""
-    var condition = true
-    var count = 0
-    for (i in 0 until description.count()) {
-        if (description[count].toString() == ";") {
-            count += 2
-            if (strPrice.toDouble() > maxPrice) {
-                maxPrice = strPrice.toDouble()
-                maxName = name
+    val pattern = Regex("""( *\D+ (\d*\.\d*|\d*);*)+""")
+    var pairOfProduct: Pair<String, Double>
+    val listOfProdutcs: List<String>
+    var maxPrice = -1.0
+    var expensiveProduct = ""
+    if (pattern.containsMatchIn(description)) {
+        if (";" in description) {
+            listOfProdutcs = description.split("; ").toMutableList()
+            for (stringProduct in listOfProdutcs) {
+                pairOfProduct = stringProduct.split(" ")[0] to stringProduct.split(" ")[1].toDouble()
+                if (pairOfProduct.second > maxPrice) {
+                    maxPrice = pairOfProduct.second
+                    expensiveProduct = pairOfProduct.first
+                }
             }
-            name = ""
-            strPrice = ""
-            condition = true
-        }
-        if (description[count].toString() == " ") condition = false
-        if (condition) {
-            name += description[count]
-        } else {
-            strPrice += description[count]
-        }
-        count++
-        if (count >= description.count()) break
+        } else return description.split(" ")[0]
     }
-    if (description.isNotEmpty()) {
-        if (strPrice.toDouble() >= maxPrice) {
-            maxName = name
-        }
-    }
-    if (description.isEmpty()) return ""
-    return maxName
+    return expensiveProduct
 }
 
 /**

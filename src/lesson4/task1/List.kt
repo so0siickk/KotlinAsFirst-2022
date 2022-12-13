67da44b44e0d01647ffdc2a0a693bb4b8539dd4c
so-siickk <sawelysawin@yandex.ru>
2022-12-13 17:31:57 +0300

    lesson7_1

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -315,13 +315,11 @@ fun decimal(digits: List<Int>, base: Int): Int {
 fun decimalFromString(str: String, base: Int): Int {
     val list = mutableListOf<String>()
     var codeLetter: Int
-    var asciiLetterA = 'a'.code
+    val asciiLetterA = 'a'.code
     for (i in str) list.add(i.toString())
     for (i in 0 until list.count()) {
-        codeLetter = list[i].toCharArray()[0].code
-        if ((codeLetter > 86)
-            && (codeLetter < 123)
-        ) list[i] = (codeLetter + 10 - asciiLetterA).toString()
+        codeLetter = list[i].toCharArray()[0].code + 10
+        if ((codeLetter >= asciiLetterA)) list[i] = (codeLetter - asciiLetterA).toString()
     }
     return decimal(list.map { it.toInt() }, base)
 }

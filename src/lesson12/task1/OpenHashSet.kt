89f231a7739e05004914d392e740646ca2495d08
so-siickk <sawelysawin@yandex.ru>
2023-02-26 17:52:30 +0300

    lesson12_1

--- src/lesson12/task1/OpenHashSet.kt
+++ src/lesson12/task1/OpenHashSet.kt
@@ -2,6 +2,10 @@
 
 package lesson12.task1
 
+import ru.spbstu.kotlin.typeclass.kind
+import ru.spbstu.wheels.NullableMonad.map
+import java.util.Objects
+
 /**
  * Класс "хеш-таблица с открытой адресацией"
  *
@@ -20,33 +24,106 @@ class OpenHashSet<T>(val capacity: Int) {
     /**
      * Массив для хранения элементов хеш-таблицы
      */
-    internal val elements = Array<Any?>(capacity) { null }
+    internal var elements = Array<Any?>(capacity) { null }
+
 
     /**
      * Число элементов в хеш-таблице
      */
-    val size: Int get() = TODO()
+    val size: Int
+        get() {
+            var count = 0
+            for (element in elements)
+                if (element != null) count++
+            return count
+        }
 
     /**
      * Признак пустоты
      */
-    fun isEmpty(): Boolean = TODO()
+    fun isEmpty(): Boolean = (size == 0)
 
     /**
      * Добавление элемента.
      * Вернуть true, если элемент был успешно добавлен,
      * или false, если такой элемент уже был в таблице, или превышена вместимость таблицы.
      */
-    fun add(element: T): Boolean = TODO()
+    fun add(element: T): Boolean {
+        val startIndex = getIndex(element)
+        var index = getIndex(element)
+        return if ((size == capacity) || (elements[index] == element)) false
+        else {
+            if (elements[index] != null) {
+                index = (index + 1) % capacity
+                while (index != startIndex) {
+                    if (elements[index] == element) return false
+                    if (elements[index] == null) {
+                        elements[index] = element
+                        return true
+                    }
+                    index = (index + 1) % capacity
+                }
+            }
+            elements[index] = element
+            true
+        }
+    }
 
     /**
      * Проверка, входит ли заданный элемент в хеш-таблицу
      */
-    operator fun contains(element: T): Boolean = TODO()
+    operator fun contains(element: T): Boolean {
+        val startIndex = getIndex(element)
+        var index = getIndex(element)
+        return if (elements[index] == element) true
+        else {
+            if (elements[index] == null) return false
+            index = (index + 1) % capacity
+            while (startIndex != index) {
+                if (elements[index] == element) return true
+                if (elements[index] == null) return false
+                index = (index + 1) % capacity
+            }
+            false
+        }
+    }
 
     /**
      * Таблицы равны, если в них одинаковое количество элементов,
      * и любой элемент из второй таблицы входит также и в первую
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        other as OpenHashSet<*>
+        for (element in other.elements) {
+            if (element != null) {
+                val startIndex = getIndex(element as T)
+                var index = getIndex(element as T)
+                if ((this.elements[index] == null) || (this.size != other.size)) return false
+                if (element === this.elements[index]) {
+                    return true
+                } else {
+                    index = (index + 1) % capacity
+                    while (startIndex != index) {
+                        if (this.elements[index] === element) return true
+                        if (this.elements[index] == null) return false
+                        index = (index + 1) % capacity
+                    }
+                }
+            }
+        }
+        return false
+    }
+
+    override fun hashCode(): Int {
+        var hash = 0
+        for (element in elements){
+            if (element != null) {
+                hash += Objects.hash(element)
+            }
+        }
+        return hash
+    }
+
+
+    private fun getIndex(element: T): Int = (element.hashCode() % capacity)
 }
